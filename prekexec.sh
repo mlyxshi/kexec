58cb9d8daf6090206dccb8ae00f4a6bf37830241
mlyxshi <mlyxdev@gmail.com>
2022-10-28 19:35:41 -0700

    test

---
+++ prekexec.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+apt install -y wget kexec-tools
+
+wget https://github.com/mlyxshi/kexec/releases/download/latest/kexec-boot
+wget https://github.com/mlyxshi/kexec/releases/download/latest/initrd.gz
+wget https://github.com/mlyxshi/kexec/releases/download/latest/bzImage
+
+chmod +x ./kexec-boot
+./kexec-boot
