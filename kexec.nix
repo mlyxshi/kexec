25c2b26df7783e692eb18c558acc8b97f902b842
mlyxshi <mlyxdev@gmail.com>
2022-10-28 23:01:23 -0700

    test

--- kexec.nix
+++ kexec.nix
@@ -1,5 +1,40 @@
 # https://github.com/NickCao/netboot/blob/master/flake.nix
-{ pkgs, modulesPath, ... }: {
+{ pkgs, modulesPath, ... }: 
+let
+  # Usage: 
+  # install github:mlyxshi/flake hk1 https://linkto/sops/key
+  install = pkgs.writeShellApplication {
+    name = "install";
+    text = ''
+      FLAKE_URL=$1
+      HOST_NAME=$2
+      KEY_URL=$3
+
+      sfdisk /dev/sda <<EOT
+      label: gpt
+      type="EFI System",        name="BOOT",  size=512M
+      type="Linux filesystem", name="NIXOS", size=+
+      EOT
+      sleep 3
+
+      mkfs.fat -F32 /dev/sda1
+      mkfs.ext4 /dev/sda2
+      mkdir /mnt
+      mount /dev/sda2 /mnt
+      mkdir /mnt/boot
+      mount /dev/sda1 /mnt/boot
+      
+      mkdir -p /mnt/var/lib/sops/   
+      curl -s "$KEY_URL" -o /mnt/var/lib/sops/age.key
+
+      nixos-install --root /mnt --flake "$FLAKE_URL"#"$HOST_NAME" \
+      --no-channel-copy --no-root-passwd \
+      --option trusted-public-keys "mlyxshi.cachix.org-1:yc7GPiryyBn0HfiCXdmO1ECWKBhfwrjdIFnRSA4ct7s= cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" \
+      --option substituters "https://mlyxshi.cachix.org https://cache.garnix.io" 
+    '';
+  };
+in
+{
   imports = [
     (modulesPath + "/profiles/minimal.nix")
     (modulesPath + "/profiles/qemu-guest.nix") # Most VPS, like oracle
@@ -25,5 +60,6 @@
 
   environment.systemPackages = [
     pkgs.htop
+    install
   ];
 }
