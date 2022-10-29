1c0147a572d0378dd67fbe93a032e8d83fdaf71f
mlyxshi <mlyxdev@gmail.com>
2022-10-28 21:50:37 -0700

    test

--- flake.nix
+++ flake.nix
@@ -1,26 +1,18 @@
 {
-  inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs ={ self, nixpkgs, ...}:
-    let
-      stateVersion = "22.05";
-    in
-    {
-      nixosConfigurations = {
-       # nix build .#nixosConfigurations.kexec.config.system.build.kexecTree
-        "kexec" = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./kexec.nix
-            {
-              system.stateVersion = stateVersion;
-            }
-          ];
-        };
-      }; 
+  outputs ={ nixpkgs, ...}:{
+    nixosConfigurations = {
+      "kexec" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./kexec.nix
+          {
+            system.stateVersion = "22.05";
+          }
+        ];
+      };
     }; 
+  }; 
+
 }
