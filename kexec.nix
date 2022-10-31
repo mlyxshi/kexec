5d6af103c06800a60acfc1dae6d21534d9f5fd55
mlyxshi <mlyxdev@gmail.com>
2022-10-30 21:38:57 -0700

    test

--- kexec.nix
+++ kexec.nix
@@ -135,7 +135,7 @@ in
   # 1 required: sshkey
   # 2 optional: install script URL
   # 3 optional: install script parameter1 -> SOPS_AGE_KEY_URL
-  # 3 optional: install script parameter2-> Telegram Bot Token
+  # 3 optional: install script parameter2 -> Telegram Bot Token
   # 3 optional: install script parameter3 -> Telegram Chat ID
   system.build.kexecScript = lib.mkForce (pkgs.writeScript "kexec-boot" ''
     #!/usr/bin/env bash
