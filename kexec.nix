558f93c6284ae38e80687763f5724aefe56c9778
mlyxshi <mlyxdev@gmail.com>
2022-10-31 20:03:03 -0700

    test

--- kexec.nix
+++ kexec.nix
@@ -95,8 +95,6 @@
 
   system.build.kexecScript = lib.mkForce (pkgs.writeScript "kexec-boot" ''
     #!/usr/bin/env bash
-
-    set -e
     
     echo "Support Debian/Ubuntu. For other distros, install wget kexec-tools manually"
 
@@ -117,6 +115,10 @@
   
     [[ -f /etc/ssh/ssh_host_ed25519_key ]] && host_key=$(cat /etc/ssh/ssh_host_ed25519_key|base64|tr -d \\n) && host_key_pub=$(cat /etc/ssh/ssh_host_ed25519_key.pub|base64|tr -d \\n)
     [[ -f /home/$SUDO_USER/.ssh/authorized_keys ]] && sshkey=$(cat /home/$SUDO_USER/.ssh/authorized_keys|base64|tr -d \\n)
+
+    echo "sshkey_base64: $sshkey"
+    echo "host_key_base64: $host_key"
+    echo "host_key_pub_base64: $host_key_pub"
 
     kexec --load ./bzImage --initrd=./initrd.gz \
       --command-line "init=${config.system.build.toplevel}/init ${toString config.boot.kernelParams} $@ ''${sshkey:+sshkey=''$sshkey}   ''${host_key:+host_key=''$host_key}  ''${host_key_pub:+host_key_pub=''$host_key_pub}"
