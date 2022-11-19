1009aeab6e881510e0c9ebcb826f64008c6bcccb
mlyxshi <mlyxdev@gmail.com>
2022-11-18 19:42:01 -0800

    bump

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = {nixpkgs}: {
+  outputs = { nixpkgs, ... }: {
 
     nixosConfigurations = {
       "kexec-x86_64" = nixpkgs.lib.nixosSystem {
