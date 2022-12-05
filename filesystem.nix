011bb61d50b865848751c6cafdbebca073071608
mlyxshi <mlyxdev@gmail.com>
2022-12-04 19:16:08 -0800

    test

--- filesystem.nix
+++ filesystem.nix
@@ -9,7 +9,7 @@
 
   fileSystems."/nix/.ro-store" = {
     fsType = "squashfs";
-    device = "../nix-store.squashfs";
+    device = "/nix-store.squashfs";
     options = [ "loop" ];
     neededForBoot = true;
   };
@@ -44,13 +44,23 @@
 
 
   # Create the squashfs image that contains the Nix store.
-  system.build.squashfsStore = pkgs.callPackage ./make-squashfs.nix {
-    storeContents = [ config.system.build.toplevel ];
+  system.build.squashfsStore = pkgs.stdenv.mkDerivation {
+    name = "nix-store.squashfs";
+    nativeBuildInputs = [ pkgs.squashfsTools ];
+    buildCommand = ''
+      closureInfo=${pkgs.closureInfo { rootPaths = config.system.build.toplevel; }}
+      # Also include a manifest of the closures in a format suitable for nix-store --load-db.
+      cp $closureInfo/registration nix-path-registration
+      mksquashfs nix-path-registration $(cat $closureInfo/store-paths) $out \
+        -no-hardlinks -keep-as-directory -all-root -b 1M -comp zstd -Xcompression-level 19
+    '';
   };
 
 
   # Create the initrd
   system.build.netbootRamdisk = pkgs.makeInitrdNG {
+    # config.system.build.initialRamdisk is compressed with zstd
+    # config.system.build.squashfsStore is compressed with zstd
     compressor = "cat";
     prepend = [ "${config.system.build.initialRamdisk}/initrd.zst" ];
 
